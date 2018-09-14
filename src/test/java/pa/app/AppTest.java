9b79eee064a2e753ff2aada19d717b71beae2d8f
diogo <diogomiguelc@hotmail.com>
2018-09-14 11:15:56 +0100

    mais alteracoes

--- src/test/java/pa/app/AppTest.java
+++ src/test/java/pa/app/AppTest.java
@@ -14,39 +14,29 @@ import static org.junit.Assert.assertTrue;
  */
 public class AppTest 
 {
-	@BeforeEach 
-	public void init() {
-		//System.out.println("@BeforeEach - Runbefore Each method");
-	}	
-	
+		
     @DisplayName("First Test - concat")
 	@Test
 	void testConcat1() {
-		App ap = new App();String res = ap.concat("Hello"," world1!");assertEquals("Hello world1!", res);
-	}	
+		App ap = new App();String res = ap.concat("Hello"," world1!");assertEquals("Hello world1!", res);}	
 	@DisplayName("Second Test - addition")
 	@Test
 	void testAddition() {
-		App ap = new App();int res = ap.addition(2,2);assertEquals(4, res);
-	}	
+		App ap = new App();int res = ap.addition(2,2);assertEquals(4, res);}	
 	@DisplayName("Third Test - subtraction")
 	@Test
 	void testSubtraction() {
-		App ap = new App();int res = ap.subtraction(4,2);assertEquals(2, res);
-	}
+		App ap = new App();int res = ap.subtraction(4,2);assertEquals(2, res);}
 	@DisplayName("Fourth Test - multiplication")
 	@Test
 	void testMultiplication() {
-		App ap = new App();int res = ap.multiplication(4,2);assertEquals(8, res);
-	}
+		App ap = new App();int res = ap.multiplication(4,2);assertEquals(8, res);}
 	@DisplayName("Fifth Test - division")
 	@Test
 	void testDivision() {
-		App ap = new App();int res = ap.division(4,2);assertEquals(2, res);
-	}
+		App ap = new App();int res = ap.division(4,2);assertEquals(2, res);}
 	@DisplayName("Sixth Test - main")
 	@Test
 	void testMain(){
-		App.main(null);assertTrue(true);
-	}
+		App.main(null);assertTrue(true);}
 }
